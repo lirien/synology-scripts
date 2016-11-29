0a125733838ed97485d7f52fec0d60f2f8b96067
Erin Sullivan <kittylily@gmail.com>
2016-11-28 22:27:42 -0800

    📂 Move the files to the correct folder

--- sort_anime.sh
+++ sort_anime.sh
@@ -1,3 +1,12 @@
 # Match mkv or mp4 starting with sub group name in brackets
+regex='.*\]\s*\(.*\) - [0-9].*'
 find /var/services/video/unsorted \
-\( -regex ".*/\[.*\].*\.mkv" -o -regex ".*/\[.*\].*\.mp4" \)  -print
+\( -regex "$regex.mkv" -o -regex "$regex.mp4" \) \
+-exec echo {} > tmp \;
+
+while read file; do
+  series=`echo $file | sed "s/$regex/\1/"`
+  echo "$series"
+  mkdir -p "/var/services/video/anime/$series"
+  mv "$file" "/var/services/video/anime/$series"
+done <tmp
